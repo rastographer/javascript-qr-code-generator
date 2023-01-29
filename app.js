073c26739d9da682d9a37cea31cdca61785dac12
rastographer <117541100+rastographer@users.noreply.github.com>
2023-01-29 21:47:58 +0300

    final commit

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 let imgBox = document.getElementById("imgBox");
 let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
+let qrMsg = document.getElementById("qrMsg");
 
 function generateQR(){
-    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
-
-    imgBox.classList.add("show-img");
+    if(qrText.value == ""){
+        qrMsg.textContent = 'Kindly input Text or URL to get QR code'; 
+        qrText.classList.add('error');
+        setTimeout(()=> {
+            qrText.classList.remove('error')
+        }, 1000)     
+    } else {
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+        qrMsg.textContent = "";
+        imgBox.classList.add("show-img");
+    }
 }
